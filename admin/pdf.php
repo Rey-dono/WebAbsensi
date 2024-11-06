d0dce44043a5c42a9607caf4ef8d82a4fa619b60
Rey-dono <yusuptilil@gmail.com>
2024-11-06 21:37:12 +0700

    pdf prototype

---
+++ admin/pdf.php
@@ -0,0 +1,86 @@
+
+<?php
+
+session_start();
+include '../all/connection.php';
+if (!isset($_SESSION['email'])) {
+    header("location: ../login-done/login71.php");
+    exit();
+}
+
+$email = $_SESSION['email'];
+
+// Fetch user data based on email
+$banyak_admin = $conn->query("SELECT * FROM user WHERE email = '$email'");
+$i =1;
+while($data=mysqli_fetch_array($banyak_admin)){
+    $nis = $data['nis'];
+    $nama = $data['nama'];
+    $kelas= $data['kelas'];
+    $status= $data['status'];
+    $waktu= $data['waktu'];
+   
+}
+
+?>
+<html>
+<head>
+  <title>Riwayat Absensi</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.css">
+  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/buttons/1.6.5/css/buttons.dataTables.min.css">
+  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
+  <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js"></script>
+</head>
+
+<body>
+<div class="container">
+			<h2>Riwayat Presensi</h2>
+			
+					
+                <table class="student-table" id="mauexport">
+                <thead>
+                    <tr>
+                        <th>NIS</th>
+                        <th>Nama</th>
+                        <th>Kelas</th>
+                        <th>Status</th>
+                        <th>Waktu</th>
+                    </tr>
+                </thead>
+               
+            </table>
+					
+				</div>
+</div>
+	
+<script>
+$(document).ready(function() {
+    $('#mauexport').DataTable( {
+        dom: 'Bfrtip',
+        buttons: [
+            'copy','csv','excel', 'pdf', 'print'
+        ]
+    } );
+} );
+
+</script>
+
+<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.6.5/js/dataTables.buttons.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.flash.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.html5.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.print.min.js"></script>
+
+	
+
+</body>
+
+</html>
